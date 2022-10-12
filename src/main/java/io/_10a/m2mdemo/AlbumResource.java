35315495f41c3e71b75f410fd063f800ffd42148
Tomasz Knyziak <t.knyziak@gmail.com>
2022-10-12 15:28:54 +0200

    Transakcje

--- src/main/java/io/_10a/m2mdemo/AlbumResource.java
+++ src/main/java/io/_10a/m2mdemo/AlbumResource.java
@@ -1,26 +1,36 @@
 package io._10a.m2mdemo;
 
+import java.util.Collections;
 import java.util.List;
 import java.util.stream.Collectors;
 import javax.inject.Inject;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
+import javax.transaction.HeuristicMixedException;
+import javax.transaction.HeuristicRollbackException;
+import javax.transaction.NotSupportedException;
+import javax.transaction.RollbackException;
+import javax.transaction.SystemException;
+import javax.transaction.TransactionManager;
+import javax.transaction.Transactional;
 import javax.ws.rs.GET;
 import javax.ws.rs.NotFoundException;
+import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 
 @Path("/album")
+@Produces(MediaType.APPLICATION_JSON)
 public class AlbumResource {
 
     @Inject EntityManager em;
 
+//    @Inject TransactionManager transactionManager;
 
     @GET
     @Path("/{id}")
-    @Produces(MediaType.APPLICATION_JSON)
     public AlbumDTO getAlbum(@PathParam("id") Long albumId) {
         final Album album = em.find(Album.class, albumId);
         if (album == null) {
@@ -37,4 +47,36 @@ public class AlbumResource {
 
         return albumDTO;
     }
+
+    @PUT
+    @Path("/new")
+    @Transactional()
+    public AlbumDTO saveNewAlbum(AlbumDTO albumDTO) {
+
+        Album album = new Album();
+        album.setTitle(albumDTO.title());
+
+
+//        try {
+//            transactionManager.begin();
+
+            em.persist(album);
+            em.flush();
+
+//            transactionManager.commit();
+//        } catch (Exception e) {
+//            try {
+//                transactionManager.rollback();
+//            } catch (SystemException ex) {
+//                throw new RuntimeException(ex);
+//            }
+//            throw new RuntimeException("Transaction error", e);
+//        }
+
+        return new AlbumDTO(album.getId(), album.getTitle(), Collections.emptyList());
+
+
+    }
+
+
 }
